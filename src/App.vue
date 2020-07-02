aeb2bd004e9a6511a52b01e842a384693dc6ce0f
craftycram <github@craftycram.net>
2020-07-02 10:43:46 +0200

    added queue submodule

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Layout></Layout>
+    <Queue></Queue>
   </div>
 </template>
 
 <script>
 import Layout from './submodules/base_layout/src/components/interface.vue';
+import Queue from './submodules/queue/src/components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
     Layout,
+    Queue,
   },
 };
 </script>
